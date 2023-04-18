db8ab52e3027a3cdcd65cdd5b5fb1759014f3d6d
VincenzoTavassi <info@egadivacanze.it>
2023-04-18 15:18:27 +0200

    types: model, migration, controller, seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $type = new Type;
+            $type->title = $faker->word();
+            $type->color = $faker->hexColor();
+            $type->save();
+        }
+    }
+}
