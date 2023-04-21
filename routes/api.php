c09d7dc1cba863783e3189568f8c73ba2506ef76
VincenzoTavassi <info@egadivacanze.it>
2023-04-21 19:30:18 +0200

    route update

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Api\ProjectController as ApiProjectController;
 |
 */
 
-Route::apiResource('/project', ApiProjectController::class);
+Route::apiResource('/projects', ApiProjectController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
