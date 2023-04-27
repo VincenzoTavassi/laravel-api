78930fe94831d37e7147d68a9e1de8918cf31c4a
VincenzoTavassi <info@egadivacanze.it>
2023-04-27 16:45:51 +0200

    mail markdown template

--- resources/views/emails/project-published.blade.php
+++ resources/views/emails/project-published.blade.php
@@ -1,2 +1,26 @@
-<h1>Progetto pubblicato</h1>
-Il progetto {{$project->title}} è stato pubblicato
+<x-mail::message>
+# Progetto: {{$project->title}}
+ 
+Il progetto è stato pubblicato!
+
+<strong>Data</strong>: {{$project->date}}<br>
+<strong>Contenuto</strong>: {{$project->description}}<br>
+<strong>Link</strong>: {{$project->link}}<br>
+ 
+<x-mail::button :url="$project->link">
+Apri il progetto
+</x-mail::button>
+ 
+A presto,<br>
+{{ config('app.name') }}
+</x-mail::message>
+
+{{-- <h1>Progetto pubblicato</h1>
+
+Il progetto {{$project->title}} è stato pubblicato.
+<hr>
+<strong>Data</strong>:{{$project->date}}
+<hr>
+<strong>Contenuto</strong>: {{$project->description}}
+<hr>
+<strong>Link</strong>:{{$project->link}} --}}
